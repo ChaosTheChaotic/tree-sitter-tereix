af957fe64bb5723d32ef02cd6a3ea36ad343d69c
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-05-09 21:09:06 +0100

    Add nix flakes in

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.gcc
+            pkgs.python3
+            pkgs.nodejs
+            pkgs.pnpm
+            pkgs.tree-sitter
+            pkgs.go
+            pkgs.cargo
+            pkgs.rustc
+          ];
+        };
+      }
+    );
+}
