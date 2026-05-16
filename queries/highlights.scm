c5ee25408970b92168c398c438d1299b9313bd79
ChaosTheChaotic <james.moriaty.ot@gmail.com>
2026-05-16 11:38:44 +0100

    Add missing items to highlights file

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,6 +1,8 @@
 ; Keywords
 ["use" "extern" "struct" "union" "enum" "if" "else" "while" "for" "switch" "case" "default" "defer" "ret" "break" "continue" "async" "inline" "static" "threadlocal" "mut"] @keyword
 
+(use_statement "as" @keyword)
+
 ; Types
 (primitive_type) @type.builtin
 (type (identifier) @type)
@@ -8,8 +10,28 @@
 (member_access base: ("self") @variable.builtin)
 (function_call caller: ("self") @variable.builtin)
 
-; Functions and Variables
+; Struct, union, enum definitions
+(struct_definition (identifier) @type)
+(union_definition (identifier) @type)
+(enum_definition (identifier) @type)
+
+; Enum members
+(enum_member (identifier) @constant)
+
+; Function definitions and calls
 (function_definition (identifier) @function)
+(function_call caller: (identifier) @function)
+
+; Member access without call
+(member_access member: (identifier) @property)
+
+; Method calls: obj.method()
+(function_call caller: (member_access member: (identifier) @function))
+
+; Use statement alias
+(use_statement "as" (identifier) @namespace)
+
+; Variables and parameters
 (variable_declaration (identifier) @variable)
 (parameter (identifier) @variable.parameter)
 
@@ -22,7 +44,8 @@
 
 ; Punctuation/Operators
 ["(" ")" "[" "]" "{" "}"] @punctuation.bracket
-["=" "+" "-" "*" "/" "%" "==" "!=" "<" ">" "<=" ">=" "&&" "||" "!" "&" "|" "^" "<<" ">>"] @operator
+["=" "+" "-" "*" "/" "%" "==" "!=" "<" ">" "<=" ">=" "&&" "||" "!" "&" "|" "^" "<<" ">>" 
+ "++" "--" "+=" "-=" "*=" "/=" "%=" "^=" "<<=" ">>="] @operator
 ";" @punctuation.delimiter
 "." @punctuation.delimiter
 
